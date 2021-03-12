63bf838119e4401c413a2adf99b00b9e496ccdde
Evilnood <evilnood666@gmail.com>
2021-03-12 11:58:12 +0200

    Добавьте файлы проекта.

---
+++ Template.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+template <class AddTemplate>
+AddTemplate sum(AddTemplate x, AddTemplate y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int a = 44, b = 25, iresult;
+    double j = 5.78, k = 3.98, dresult;
+    iresult = sum(a, b);
+    dresult = sum(j, k);
+    cout << "Result of adding int numbers: " << iresult<<endl;
+    cout << "Result of adding double numbers: " << dresult;
+}
